7ee2fcf8952f0872d060fb663832d13ea92d89e1
BigMoistLochu <krok.konrad1@gmail.com>
2025-03-19 16:10:52 +0100

    added dockerfile, docker-compose and added basic configuration for project

--- PetsyBackend/src/main/java/application/petsybackend/PetsyBackendApplication.java
+++ PetsyBackend/src/main/java/application/petsybackend/PetsyBackendApplication.java
@@ -1,13 +1,18 @@
 package application.petsybackend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PetsyBackendApplication {
+public class PetsyBackendApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(PetsyBackendApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Zmienne srodowiskowe: " + System.getenv());
+    }
 }
