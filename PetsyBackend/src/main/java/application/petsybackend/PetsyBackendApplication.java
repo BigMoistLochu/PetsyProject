8376c1448e6f0bc0bbd04e1f771d8ff78354e8ed
BigMoistLochu <krok.konrad1@gmail.com>
2025-03-19 15:31:20 +0100

    init backend project, added basic gitignore

---
+++ PetsyBackend/src/main/java/application/petsybackend/PetsyBackendApplication.java
@@ -0,0 +1,13 @@
+package application.petsybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetsyBackendApplication.class, args);
+    }
+
+}
---
+++ PetsyBackend/src/test/java/application/petsybackend/PetsyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package application.petsybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
