0ae4754180b8f3200b58732c320d3a971a7bef22
BigMoistLochu <krok.konrad1@gmail.com>
2025-03-31 15:21:24 +0200

    added advertisment component, add service to show all advertisment

---
+++ PetsyFrontend/src/js/components/advertisementsComponent.js
@@ -0,0 +1,11 @@
+import { getAllAdvertisements } from "../services/AdvertisementService.js";
+
+
+export function showAllOgloszenia(){
+    console.log("xdfdfad");
+    const data = getAllAdvertisements();
+}
+
+function ape(){
+    console.log("ADD");
+}
--- PetsyFrontend/src/js/services/AdvertisementService.js
+++ PetsyFrontend/src/js/services/AdvertisementService.js
@@ -1,15 +1,14 @@
-async function getAllAdvertisements(){
+export async function getAllAdvertisements(){
     try{
         const response = await fetch("http://localhost:8080/api/v1/advertisement",{
             method: "GET"
         });
 
-        if (!response.ok) {
-            throw new Error("Cos poszlo nie tak");
-        }
+        if (!response.ok) throw new Error("Cos poszlo nie tak");
 
         const data = await response.json();
-        console.log("O to ogloszenia:", data);
+        console.log(data);
+        return data;
     }catch (error){
         console.error("Błąd logowania:", error.message);
         alert("Błąd logowania: " + error.message);
